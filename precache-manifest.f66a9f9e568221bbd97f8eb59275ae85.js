3a061ad0c3ecc223caa39f711621d9f0ff5d4fb5
yash sharma <yashsharma2572@gmail.com>
2022-03-21 10:37:46 +0530

    Updates

---
+++ precache-manifest.f66a9f9e568221bbd97f8eb59275ae85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc7c64f01809235302881418367d87f",
+    "url": "/React-Reploy/index.html"
+  },
+  {
+    "revision": "1cde4eaefe15b5857870",
+    "url": "/React-Reploy/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f13a0b6498955a119237",
+    "url": "/React-Reploy/static/js/2.17c7b4e6.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/React-Reploy/static/js/2.17c7b4e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cde4eaefe15b5857870",
+    "url": "/React-Reploy/static/js/main.be79c372.chunk.js"
+  },
+  {
+    "revision": "934101e02dd3484e3540",
+    "url": "/React-Reploy/static/js/runtime-main.02428fd7.js"
+  }
+]);
